3d47d1c2ba71ab12c36b8b6c036af8aebf00009e
Habert <habertotieno4@gmail.com>
2025-03-28 03:41:15 +0300

    updated index.js

--- js/index.js
+++ js/index.js
@@ -1,8 +1,24 @@
-let formInfo=document.getElementById("submit")
-formInfo.addEventListener('submit',(event)=>{
-event.preventDefault
 
-}
+let baseUrl = 'http://localhost:3000/books';
 
-)
+fetch(baseUrl)
+  .then(function(response) {
+    if (!response.ok) {
+      throw  error(`HTTP error! Status: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then(function(books) {
+    console.log('Books fetched:', books);
+    displayBooks(books);
+  })
+  .catch(function(error) {
+    console.error('Error fetching books:', error);
+  });
+
+
+
+
+
+
 
---
+++ readMe.md
@@ -0,0 +1,3 @@
+#Book review Project#
+
+
